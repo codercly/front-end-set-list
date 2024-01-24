d917ee03ef328c3c74fa25d6c5eb54e751343bb8
codercly <rrclementirafael@gmail.com>
2024-01-23 21:07:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function isValidSpotifyPlaylistLink(link) {
+  const spotifyRegex = /^https:\/\/open\.spotify\.com\/playlist\/[a-zA-Z0-9]+(\?|$)/;
+  return spotifyRegex.test(link);
+}
+
 function App() {
+  const [playlistLink, setPlaylistLink] = useState('');
+  const [lyrics, setLyrics] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [expandedSong, setExpandedSong] = useState(null);
+
+  const handleGetLyrics = async () => {
+    try {
+      if (!isValidSpotifyPlaylistLink(playlistLink)) {
+        setError('Link inválido. Certifique-se de que é um link de playlist do Spotify.')
+        return;
+      }
+
+      setLoading(true);
+      setError(null);
+
+      const response = await fetch('http://localhost:5000/api/get_lyrics', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ playlist_link: playlistLink }),
+      });
+
+      const data = await response.json();
+      setLyrics(data.lyrics);
+    } catch (error) {
+      console.error('Error fetching lyrics:', error);
+      setError('Erro ao obter as letras. Tente novamente mais tarde.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleExpandSong = (index) => {
+    setExpandedSong(index === expandedSong ? null : index)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Canta Saporra Direito MORTÂO!!!</h1>
+      <input className='inp'
+        type="text"
+        placeholder="Enter Spotify Playlist Link"
+        value={playlistLink}
+        onChange={(e) => setPlaylistLink(e.target.value)}
+      />
+      <button className='btn' onClick={handleGetLyrics}>Get Lyrics</button>
+
+      {isLoading ? (
+        <p style={{color:'#e75d00'}}>Calma ae microfone! Estou pegando as letras</p>
+      ) : (
+        <div>
+          {error && <p style={{ color: 'red' }}>{error}</p>}
+          {lyrics.map((song, index) => (
+            <div className='songname' key={index}>
+              <h3 onClick={() => handleExpandSong(index)} style={{ cursor: 'pointer' }}>
+                {song.name}
+              </h3>
+              {expandedSong === index && (
+                <div>
+                  <p>{song.artists}</p>
+                  <pre>{song.lyrics}</pre>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
